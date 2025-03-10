bff62ffa0314b5cb30ca843b423a6b3545164acc
hheeseung <ssigu69@gmail.com>
2025-03-10 16:51:32 +0900

    feat: 화면 상단 네비게이션 바 추가 및 하단 footer 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+export default function Footer() {
+  return (
+    <footer className='py-2 border-t border-gray-200'>
+      <FaGithub className='mx-auto text-2xl' />
+    </footer>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='flex justify-between items-center py-2'>
+      <h1 className='font-bold text-2xl'>
+        <Link href='/'>Hlog</Link>
+      </h1>
+      <ul className='flex items-center justify-evenly space-x-4'>
+        <Link href='/'>Home</Link>
+        <Link href='/posts'>Posts</Link>
+        <Link href='/about'>About</Link>
+      </ul>
+    </header>
+  );
+}
