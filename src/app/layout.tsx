e886cea35fc7dcdb3e252612fd65bddc9330d969
hheeseung <ssigu69@gmail.com>
2025-04-01 15:13:13 +0900

    fix: og 태그 이미지 안뜨는 문제 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     title: '하희승 | 성장하는 개발자',
     description: '광활한 웹을 헤엄치며 꾸준히 상향곡선을 그리기 위해 노력하는 개발자입니다.',
     images: {
-      url: '/images/avatar.jepg',
+      url: '/images/avatar.jpeg',
     },
   },
 };
